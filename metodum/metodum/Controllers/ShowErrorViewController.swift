a4cc1e1e7b740ab9fb2cba6f7ce1254a3d992b5e
ivanportales <gidsp.snf19@uea.edu.br>
2020-10-05 10:15:20 -0400

    added error screen and assets

---
+++ metodum/metodum/Controllers/ShowErrorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShowErrorViewController.swift
+//  metodum
+//
+//  Created by Gonzalo Ivan Santos Portales on 05/10/20.
+//  Copyright © 2020 metodum. All rights reserved.
+//
+
+import UIKit
+
+class ShowErrorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
