37ccc3f719a05f63f128759b791a3f0e9310eedc
guilherme <jguicf@gmail.com>
2020-10-07 11:24:39 -0400

    unfinished favorite screen

---
+++ metodum/metodum/Controllers/FavoritesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FavoritesViewController.swift
+//  metodum
+//
+//  Created by João Guilherme on 07/10/20.
+//  Copyright © 2020 metodum. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
