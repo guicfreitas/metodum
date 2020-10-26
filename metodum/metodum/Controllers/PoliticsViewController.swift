4efef5d642f87ac49e21d5b23b22d3fe6c1dba74
guilherme <jguicf@gmail.com>
2020-10-26 11:34:50 -0400

    added onboarding screens

---
+++ metodum/metodum/Controllers/PoliticsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PoliticsViewController.swift
+//  metodum
+//
+//  Created by João Guilherme on 26/10/20.
+//  Copyright © 2020 metodum. All rights reserved.
+//
+
+import UIKit
+
+class PoliticsViewController: UIViewController {
+    @IBOutlet weak var acceptButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        acceptButton.layer.cornerRadius = 20
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
