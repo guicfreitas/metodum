64e673aa33694cdd74ae48408ef4a9cc98ee5f82
guilherme <jguicf@gmail.com>
2020-10-29 17:56:47 -0400

    fixed onboarding bugs

--- metodum/metodum/Controllers/PoliticsViewController.swift
+++ metodum/metodum/Controllers/PoliticsViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class PoliticsViewController: UIViewController {
+    @IBOutlet weak var termsButton: UIButton!
     @IBOutlet weak var acceptButton: UIButton!
+    @IBOutlet weak var privButton: UIButton!
+    let language = Locale.current.languageCode
+    
+    var attrs = [
+        NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18, weight: .semibold),
+        NSAttributedString.Key.foregroundColor : UIColor.white,
+        NSAttributedString.Key.underlineStyle : 1] as [NSAttributedString.Key : Any]
+    
+    var attributedStringTerms = NSMutableAttributedString(string:"")
+    var attributedStringPriv = NSMutableAttributedString(string:"")
+
     var callback : (() -> ())?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let buttonTitleTerms = NSMutableAttributedString(string:(language == "pt") ? "termos de uso" : "terms of use", attributes:attrs)
+        let buttonTitlePriv = NSMutableAttributedString(string: (language == "pt") ? "política de privacidade" : "privacy policy", attributes:attrs)
+        attributedStringTerms.append(buttonTitleTerms)
+        attributedStringPriv.append(buttonTitlePriv)
+        
+        termsButton.setAttributedTitle(attributedStringTerms, for: .normal)
+        privButton.setAttributedTitle(attributedStringPriv, for: .normal)
         acceptButton.layer.cornerRadius = 10
     }
     
     @IBAction func acceptedButtonPressed(_ sender: Any) {
         navigationController?.popToRootViewController(animated: false)
         callback?()
+    }
+
+
+    @IBAction func termsPressed(_ sender: Any){
+        performSegue(withIdentifier: "terms", sender: nil)
+        
+    }
+    
+    @IBAction func privPressed(_ senedr: Any){
+        performSegue(withIdentifier: "priv", sender: nil)
+        
     }
 }
 
